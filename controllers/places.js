d43d4d6a9935e43b3cb85da1425bcbc9d238f21a
jake <jakebrown536@gmail.com>
2022-11-28 21:30:42 -0700

    added alot

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,57 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'la esquina',
-        city: 'las Vegas',
-        state: 'Nevada',
-        Cuisines: 'Mexican',
-        pic: '/images/resturant.png'
-    },{
-        name: 'Coding cat cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        Cuisines: 'coffie, bakery',
-        pic: '/images/resturant2.png'
-    }]
     res.render('places/index', { places })
 })
 
+//show
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/250/250'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('POST /places')
+  })
+  
+  router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id,1)
+      res.redirect('/places')
+    }
+  })
+  
+
+  
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,15 @@
+let places = [{
+    name: 'la esquina',
+    city: 'las Vegas',
+    state: 'Nevada',
+    Cuisines: 'Mexican',
+    pic: '/images/resturant.png'
+},{
+    name: 'Coding cat cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    Cuisines: 'coffie, bakery',
+    pic: '/images/resturant2.png'
+}]
+
+module.exports = places
