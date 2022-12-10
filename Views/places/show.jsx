86cbf92d3c5f761c3223cb5c210ff202e4937012
jake <jakebrown536@gmail.com>
2022-12-10 10:57:37 -0700

    added the ability to leave comments on pages

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+  let comments = (
+    <h3 className='inactive'>
+      no comments yet!
+    </h3>
+  )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
   return (
     <Def>
       <main>
@@ -47,6 +66,7 @@ function show(data) {
           <h2>Comments</h2>
           <h4>no comments yet!</h4>
         </div>
+        {comments}
       </main>
     </Def>
   )
--- controllers/places.js
+++ controllers/places.js
@@ -44,14 +44,17 @@ router.get('/new', (req, res) => {
 // Show = catch and error?
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-    .then(place => {
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
-    })
-    .catch(err => {
+  })
+  .catch(err => {
       console.log('err', err)
       res.render('error404')
-    })
+  })
 })
+
 
 //  PUT route
 router.put('/:id', (req, res) => {
@@ -87,6 +90,29 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
     })
 })
+// comment route
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
